e57561c7e1e28e60239582bebc6ca5507d2dcbe5
Prajakta Sharad Ghige <109867537+prajaktaghige@users.noreply.github.com>
2024-12-06 03:57:29 +0000

    Variables and datatypes done.

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+//"Use Strict" //treat all IS code as newer version
+//alert(3+3)  //we are using node js not browser
+//console.log(3+3); console.log("Prajakta");
+
+
+/* There are many types of datatype
+1.Primitive Datatypes
+number=>2 to power of 54, 
+bigint,
+String=>''or"", 
+boolean,\
+undefined,
+null=>standalone value,
+symbol=>for finding uniquness 
+
+2.Objects
+console.log(typeOf undefined); //undefined
+console.log(typeOf null); //Object
+
+*/
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId=145554
+let accountEmail="prajakta@google.com"
+var accountPassword="12345"
+accountCity="Pune"
+let accountState;
+
+//accountId=2
+accountEmail="abc@gmail.com"
+accountPassword="16"
+accountCity="Mumbai"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+/* 
+Prefer not to use var 
+beacuse of issue in block scope and functional scope
+*/
+
