24f2e5c3545b96f1a830e2dc98a3f39f5ae0af21
Prajakta Sharad Ghige <109867537+prajaktaghige@users.noreply.github.com>
2024-12-06 04:20:21 +0000

    checking

--- 01_basics/01_conversionOperation.js
+++ 01_basics/01_conversionOperation.js
@@ -1,32 +1,75 @@
-let score="33"
+// let score="33"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber=Number(score)
-console.log(valueInNumber);
+// let valueInNumber=Number(score)
+// console.log(valueInNumber);
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
-/*"33"=> 33
-"33abc" =>NaN
-true=>1; false =>0
-null=>0; undefined=>undefined
-*/
+// /*"33"=> 33
+// "33abc" =>NaN
+// true=>1; false =>0
+// null=>0; undefined=>undefined
+// */
 
-let isLoggedIn = 1
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);//output will be true
+// let isLoggedIn = 1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);//output will be true
 
-/*
-1=> true
-0=>false
-""=>false
-"name"=>true
-*/
+// /*
+// 1=> true
+// 0=>false
+// ""=>false
+// "name"=>true
+// */
 
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+
+//******************************Operation******************** */
+let value=3
+let negValue= -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1="hello"
+// let str2="Prajakta"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(1+"2"+2)
+
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+// let num1, num2 ,num3
+// num1=num2=num3=2+2
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
 
