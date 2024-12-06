c43973d47951a2bb0558b5b123405a093deed8d4
Prajakta Sharad Ghige <109867537+prajaktaghige@users.noreply.github.com>
2024-12-06 04:51:28 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+// primitive datatypes
+// 7types:String,Number,Boolean,null,undefined,symbol, BigInt
+
+const score=100
+const scoreValue=200320.9
+const isLooedIn=false
+const outsideTemp=null
+let userEmail;
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id === anotherId)
+const bigNumber=45048390840958495789454058498n
+
+// Reference/ non primitive datatype
+// Array,Object,Function
+
+const heros=["shaktiman","naagraj","doga"]
+
+let myObj={
+    name:"prajakta",
+    age:22
+}
+
+const myFunction=function(){
+    console.log("Hello wold !!");
+    
+}
+
+console.log(typeof bigNumber);
